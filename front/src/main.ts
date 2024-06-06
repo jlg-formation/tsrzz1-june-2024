588f56680af23c685a32ec57a60961530fd915e3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-06 17:13:24 +0200

    dev: samples ok

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,15 +1,24 @@
 import "./style.css";
 
+const cx0 = 50;
+const cy0 = 50;
+const r0 = 45;
+
+const samples = 10;
+
 const svgns = "http://www.w3.org/2000/svg";
 const container = document.querySelector("g.samples");
 if (container === null) {
   throw new Error("oups");
 }
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < samples; i++) {
   const circle = document.createElementNS(svgns, "circle");
-  const x = 34;
-  const y = 89;
+
+  const angle = i * ((Math.PI * 2) / samples) + Math.PI / 2;
+  const x = cx0 + r0 * Math.cos(angle);
+  const y = cy0 + r0 * Math.sin(angle);
+
   circle.setAttributeNS(null, "cx", x + "");
   circle.setAttributeNS(null, "cy", y + "");
   circle.setAttributeNS(null, "r", "1");
