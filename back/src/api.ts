33d626ea12b2a94998e8e948825a643e3397fe73
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-06 15:24:49 +0200

    dev: added random

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import { random } from './utils';
 
 const app = express.Router();
 
 app.get('/random-config', (req, res) => {
-  res.json({ samples: 12, multiplicationFactor: 123.45 });
+  res.json({
+    samples: random(0, 500),
+    multiplicationFactor: random(0, 100, 2),
+  });
 });
 
 export default app;
