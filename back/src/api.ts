db7a962454f16dbff6e386e7b3594abc6d1bc1f8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-06 15:32:06 +0200

    dev: back finished

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import { random } from './utils';
+import { Config } from './interfaces/Config';
 
 const app = express.Router();
 
 app.get('/random-config', (req, res) => {
-  res.json({
+  const config: Config = {
     samples: random(0, 500),
     multiplicationFactor: random(0, 100, 2),
-  });
+  };
+  res.json(config);
 });
 
 export default app;
