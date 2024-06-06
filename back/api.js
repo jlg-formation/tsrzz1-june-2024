210e4bbd69adbaa40813eca6a3bdfa8340c5dcde
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-06 12:21:19 +0200

    dev: added nodemon

--- back/api.js
+++ back/api.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express.Router();
 
 app.get('/random-config', (req, res) => {
-  res.json({ samples: 12, multiplicationFactor: 45.56 });
+  res.json({ samples: 12, multiplicationFactor: 123.45 });
 });
 
 module.exports = app;
