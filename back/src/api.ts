a87941a4202e0508e1e1ef8f687fd234cbe4f47a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-06 15:04:02 +0200

    dev: added typings and import notation

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 
 const app = express.Router();
 
@@ -6,4 +6,4 @@ app.get('/random-config', (req, res) => {
   res.json({ samples: 12, multiplicationFactor: 123.45 });
 });
 
-module.exports = app;
+export default app;
