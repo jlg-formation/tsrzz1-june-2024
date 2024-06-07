dcc9affccb4faaf088c5eea5e08c244a9d036e4a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-07 16:37:20 +0200

    dev: proxy config

--- front/src/constants.ts
+++ front/src/constants.ts
@@ -6,4 +6,4 @@ export const svgns = "http://www.w3.org/2000/svg";
 
 export const step = 0.01;
 
-export const url = "http://localhost:3000/api/random-config";
+export const url = "/api/random-config";
