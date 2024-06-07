d8d5e1d26113c594523a5863d67ba64941f1872f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-07 12:34:03 +0200

    dev: loop on keys

--- front/src/classes/Command.ts
+++ front/src/classes/Command.ts
@@ -1,13 +1,24 @@
 import { Config } from "../interfaces/Config";
+import { querySelector } from "../misc";
 
 type Callback = (newConfig: Config) => void;
 
 export class Command {
   callback: Callback = () => {};
 
-  constructor(private config: Config) {}
+  constructor(private config: Config) {
+    this.render();
+  }
 
   onUpdateConfig(callback: Callback) {
     this.callback = callback;
+  }
+
+  render() {
+    const keys: (keyof Config)[] = ["samples", "multiplicationFactor"];
+    for (const key of keys) {
+      querySelector(`div.command label.${key} .value`).innerHTML =
+        this.config[key] + "";
+    }
   }
 }
